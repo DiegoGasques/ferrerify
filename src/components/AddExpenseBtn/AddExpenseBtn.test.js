8e22715c938d310cb3bc92930c9f169e5c05b3f8
Diego <diegomgasques@gmail.com>
2019-07-31 15:29:10 -0300

    [TEST] Add fixtures expenses
    Tests will need to receive expenses as props in testing files

--- src/components/AddExpenseBtn/AddExpenseBtn.test.js
+++ src/components/AddExpenseBtn/AddExpenseBtn.test.js
@@ -4,15 +4,8 @@ import toJson from "enzyme-to-json";
 import { AddExpenseBtn } from "./AddExpenseBtn";
 
 describe("[COMPONENT] AddExpenseBtn", () => {
-  let wrapper;
-  beforeEach(() => (wrapper = shallow(<AddExpenseBtn />)));
-  afterEach(cleanup);
-
   it("Tests for changes in the rendered component", () => {
+    const wrapper = shallow(<AddExpenseBtn />);
     expect(toJson(wrapper)).toMatchSnapshot();
-  });
-
-  it("Tests for right path on the Link component", () => {
-    expect(wrapper.prop("to")).toBe("/new");
   });
 });
