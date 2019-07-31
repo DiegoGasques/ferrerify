1c3bfce09d5e99e71170135c3b19df151a1b1ec6
Diego <diegomgasques@gmail.com>
2019-07-31 11:20:52 -0300

    [Style] Add MainContent and SecondaryContent styles

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./MainContent.scss";
 
 export const MainContent = ({ children }) => {
   return <div className="MainContent">{children}</div>;
--- src/components/SecondaryContent/SecondaryContent.js
+++ src/components/SecondaryContent/SecondaryContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SecondaryContent.scss";
 
 export const SecondaryContent = ({ children }) => {
   return <div className="SecondaryContent">{children}</div>;
